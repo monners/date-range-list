b85f2cf9525b8a2125a1ff00b688a58456321daa
Josh Moncrieff <joshmoncrieff@me.com>
2016-02-03 21:00:06 +1100

    Get the style/css/sass loaders up and running Include the default date-picker styles (borrowed from the src)

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import DateRangePicker from 'react-daterange-picker';
 
+// Style Imports
+import './date-picker.scss';
+
 export default class DatePicker extends Component {
     render() {
         return (
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,9 @@ module.exports = {
             test: /\.jsx?$/,
             loaders: ['react-hot', 'babel'],
             include: path.join(__dirname, 'src')
+        }, {
+            test: /\.scss$/,
+            loader: 'style!css!sass'
         }]
     },
     resolve: {
